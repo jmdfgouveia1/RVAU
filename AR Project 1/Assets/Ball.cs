33e9eeab5e5ba82197d53e6dd155983df86ba5f5
Pedro Fernandes <pedro_fernandes@live.com>
2016-11-13 01:19:18 +0000

    better targets - physics

---
+++ AR Project 1/Assets/Ball.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	public Rigidbody rb;
+	public bool up;
+
+	float sx;
+	float sy;
+
+
+	// Use this for initialization
+	void Start () {
+		sx = Random.Range (0, 2) == 0 ? -1 : 1;
+		sy = Random.Range (0, 2) == 0 ? -1 : 1;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (up && Input.GetKeyDown ("space")) {
+			up = false;
+			rb.velocity = new Vector3 (Random.Range (1, 4) * sx, Random.Range (1, 4) * sy, 0);
+		}
+		//if (Input.GetKeyUp ("space"))
+		//	up = true;
+
+
+
+	}
+}
